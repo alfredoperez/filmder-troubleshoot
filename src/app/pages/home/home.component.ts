6321536ce4cd81cdc70405fce33e93dbd0971314
xavigu <xavigu91@gmail.com>
2022-09-02 13:02:04 +0200

    Add infinite scroll using the position of the scroll

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Movie, MoviesListingResponse } from 'src/app/interfaces/models';
 import { FilmsService } from 'src/app/services/films.service';
 
@@ -10,6 +10,20 @@ import { FilmsService } from 'src/app/services/films.service';
 export class HomeComponent implements OnInit {
 
   movies: Movie[] = [];
+  moviesSlideshow: Movie[] = [];
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(){
+    const pos = (document.documentElement.scrollTop || document.body.scrollTop) + 1200;
+    const max = (document.documentElement.scrollHeight || document.body.scrollHeight);
+    console.log({pos, max});
+
+    if (pos > max) {
+      this.filmsService.getMoviesListing().subscribe((movies: Movie[]) => {
+        this.movies.push(...movies);
+       });
+    }
+  }
 
   constructor(private filmsService: FilmsService) { }
 
@@ -18,8 +32,9 @@ export class HomeComponent implements OnInit {
   }
 
   getMoviesListing(){
-    this.filmsService.getMoviesListing().subscribe((response: MoviesListingResponse) => {
-      this.movies = response.results;
+    this.filmsService.getMoviesListing().subscribe((movies: Movie[]) => {
+      this.movies = movies;
+      this.moviesSlideshow = movies;
      })
   }
 
--- src/app/services/films.service.ts
+++ src/app/services/films.service.ts
@@ -1,16 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { MoviesListingResponse } from '../interfaces/models';
+import { map, Observable, of, tap } from 'rxjs';
+import { Movie, MoviesListingResponse } from '../interfaces/models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmsService {
 
+  private baseUrl: string = 'https://api.themoviedb.org/3';
+  private moviesListingPage = 1;
+  public loading: boolean = false;
+
   constructor(private http: HttpClient) { }
 
-  getMoviesListing():Observable<MoviesListingResponse>{
-    return this.http.get<MoviesListingResponse>('https://api.themoviedb.org/3/movie/now_playing?api_key=406ac682d93f72cd6bc327be30109839&language=es-ES&page=1')
+  get params() {
+    return {
+      api_key: '406ac682d93f72cd6bc327be30109839',
+      language: 'es-ES',
+      page: this.moviesListingPage.toString()
+    }
+  }
+
+  getMoviesListing():Observable<Movie[]>{
+    
+    if (this.loading) {
+      return of([]);
+    }
+    this.loading = true;
+    //increment moviesListingPage in each get call
+    return this.http.get<MoviesListingResponse>(`${this.baseUrl}/movie/now_playing`, {params: this.params}).pipe(
+      map((response) => response.results),
+      tap(() => {
+        console.log('Llamada a api')
+        this.moviesListingPage += 1;
+        this.loading = false;
+      })
+    );
   }
 }
