4fed0d033a72487da8f5049bde07b7e78f0cdf5c
xavigu <xavigu91@gmail.com>
2022-09-02 17:11:14 +0200

    add film-detail component UI

--- src/app/pages/film-detail/film-detail.component.ts
+++ src/app/pages/film-detail/film-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MovieDetailsResponse } from 'src/app/interfaces/movieDetails-models';
 import { FilmsService } from 'src/app/services/films.service';
 
 @Component({
@@ -9,16 +10,26 @@ import { FilmsService } from 'src/app/services/films.service';
 })
 export class FilmDetailComponent implements OnInit {
 
+  public film!: MovieDetailsResponse;
 
-  constructor(private activatedRoute: ActivatedRoute, private filmsService: FilmsService) { }
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    private filmsService: FilmsService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     //desestructurin the params object
     const { id } = this.activatedRoute.snapshot.params;
     console.log(id)
-    this.filmsService.getFilmDetails(id).subscribe( movie => {
-      console.log('movie details: ', movie);
+    this.filmsService.getFilmDetails(id).subscribe(movieResponse => {
+      console.log('movie details: ', movieResponse);
+      this.film = movieResponse;
     })
+  }
+
+  onBackHome(){
+    this.router.navigate(['/home']);
   }
 
 }
