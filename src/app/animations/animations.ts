aa250e8008548732554cbdf498300c03b8cf642e
xavigu <xavigu91@gmail.com>
2023-03-16 19:09:31 +0100

    Add list animation to cast-slideshow component

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -1,4 +1,13 @@
-import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes,
+  query,
+  stagger,
+} from '@angular/animations';
 
 //fade-in opacity animation.
 export const fadeIn = [
@@ -125,6 +134,64 @@ export const smoothCollapse = [
     transition('final=>initial', animate('750ms')),
   ]),
 ];
+
+// animation to use in a div with ngFor elements ([@listState]="array?.length")
+export const listAnimation = trigger('listState', [
+  transition('* => *', [
+    query(
+      ':enter',
+      [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100%)',
+        }),
+        stagger(
+          300,
+          animate(
+            '500ms ease-out',
+            keyframes([
+              style({
+                opacity: 0.6,
+                transform: 'translateX(45%)',
+                offset: 0.6,
+              }),
+              style({
+                opacity: 1,
+                transform: 'translateX(0)',
+                offset: 1,
+              }),
+            ])
+          )
+        ),
+      ],
+      { optional: true }
+    ),
+    query(
+      ':leave',
+      [
+        stagger(
+          300,
+          animate(
+            '1500ms ease-out',
+            keyframes([
+              style({
+                opacity: 0.4,
+                transform: 'translateX(50%)',
+                offset: 0.5,
+              }),
+              style({
+                opacity: 0,
+                transform: 'translateX(100%)',
+                offset: 1,
+              }),
+            ])
+          )
+        ),
+      ],
+      { optional: true }
+    ),
+  ]),
+]);
 
 // animation to route components
 export const routeAnimation = trigger('routeFadeState', [
